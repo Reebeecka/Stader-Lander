8d917b9e0da75de6b34af467216e327940f51ebb
Anton <94136841+kodareAnton@users.noreply.github.com>
2021-12-21 11:31:26 +0100

    main push
    main push

---
+++ script.js
@@ -0,0 +1,34 @@
+/// import functions
+import { getLandAndStad, hello } from './modules/fetch.mjs';
+console.log(getLandAndStad);
+
+hello();
+
+
+/// Lander.mjs
+/// function som skriver ut länder
+
+/// stader.mjs
+/// klickevent visa städer 
+/// vilket id på land har man tryckt på 
+/// skriv ut städer som har samma id
+
+/// info.mjs
+/// klickevent visa information om städerna
+/// vilket id stad man har tryckt på 
+/// visa population
+/// knapp som sparar id i local storage
+
+/// localStorage.mjs
+/// function som hämtar stadens namn och population med hjälp av id
+/// plusa ihop populationen
+/// function som skriver ut alla städer ur localstorag
+
+/// romoveLocalStorage.mjs
+/// knapp som tömmer local storage listan
+
+/// VG
+/// API 
+
+/// 
+
