ff8009f40e12eef57363bee91f5a697d7a16dade
Mari-Artho <mari.artho@medieinstitutet.se>
2021-12-21 20:04:42 +0100

    Moved code to print cities(stader.mjs).

--- modules/lander.mjs
+++ modules/lander.mjs
@@ -1,3 +1,5 @@
+import { printStad } from "./stader.mjs";
+
 // export only top-level function (printLands)
 export function printLands() {
   //Get lander's data from JSON.
@@ -18,23 +20,4 @@ function printLand(land) {
   let landUL = document.createElement("ul");
   li.append(landUL);
   printStad(landUL, land.id);
-}
-
-function printStad(landUL, id) {
-  //Get Stad's data from JSON.
-  fetch("json/stad.json")
-    .then((response) => response.json())
-    .then((stads) => stads.filter((stad) => stad.countryid == id))
-    .then((data) =>
-      data.sort(function (a, b) {
-        return a.stadname.localeCompare(b.stadname);
-      })
-    )
-    .then((data) => data.forEach((stad) => printStad(stad)));
-
-  function printStad(stad) {
-    let li = document.createElement("li");
-    landUL.append(li);
-    li.innerText = stad.stadname;
-  }
 }
