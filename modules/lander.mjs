35b75d6d67a74db9a476802ec97f46c192ddd58f
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2021-12-22 11:17:46 +0100

    Function on click
    Lagt till så att det går att trycka på länderna för att visa städerna och trycka på städerna för att visa population

--- modules/lander.mjs
+++ modules/lander.mjs
@@ -10,14 +10,25 @@ export function printLands() {
 
 // print one country at a time (called from inside the fetch chain)
 function printLand(land) {
-  let ul = document.getElementById("land");
-  let li = document.createElement("li");
-  //Jag trodde att create button är bättre än list? Vad tror ni?
-  //let li = document.createElement("button");
-  ul.append(li);
-  li.innerText = land.countryname;
-  // print each land in the array of lands
-  let landUL = document.createElement("ul");
-  li.append(landUL);
-  printStad(landUL, land.id);
+  let landNav = document.getElementById("land");
+  let landName = document.createElement("li");
+
+  //Add text to LI element with the name of the Country
+  landName.innerText = land.countryname;
+
+  // Append the Li element to the UL thats already in HTML
+  landNav.append(landName);
+
+  //Create eventlistener for every contry
+  landName.addEventListener("click", function(){
+    //Stader is an ul list in index.html and section is also in main
+    let stader = document.getElementById("stader");
+    let section = document.getElementById("section");
+    //Clear stader and section to only show one contries cities
+    stader.innerHTML="";
+    section.innerHTML="";
+    //call printStad, and send the ID of the contry you just clicked.
+    printStad(land.id);
+  })
+
 }
--- modules/stader.mjs
+++ modules/stader.mjs
@@ -1,4 +1,4 @@
-export function printStad(landUL, id) {
+export function printStad(id) {
   //Get Stad's data from JSON.
   fetch("json/stad.json")
     .then((response) => response.json())
@@ -11,8 +11,31 @@ export function printStad(landUL, id) {
     .then((data) => data.forEach((stad) => printStad(stad)));
 
   function printStad(stad) {
+    let stader = document.getElementById("stader");
+    // Creates one LI element for each city
     let li = document.createElement("li");
-    landUL.append(li);
+
+    // LI inner text is the cityname in the API
     li.innerText = stad.stadname;
+
+    //appended li to ul
+    stader.append(li);
+
+    // Adds eventlistenes to the cities
+    li.addEventListener("click", function(){
+
+      //Finds the section already in HTML and clears in to only get one city
+      let section = document.getElementById("section");
+      section.innerHTML="";
+
+      //Creates H1 and P element to wirte out the Cityname and popluation
+      let stadH1 = document.createElement("h1");
+      stadH1.innerText = stad.stadname;
+
+      let stadP = document.createElement("p");
+      stadP.innerText = stad.population;
+
+      section.append(stadH1, stadP);
+    })
   }
 }
