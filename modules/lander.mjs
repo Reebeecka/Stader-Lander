eee9dfea4f1585466ce69bcf501ba17295caabcc
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2021-12-22 10:18:27 +0100

    Update lander.mjs

--- modules/lander.mjs
+++ modules/lander.mjs
@@ -11,9 +11,9 @@ export function printLands() {
 // print one country at a time (called from inside the fetch chain)
 function printLand(land) {
   let ul = document.getElementById("land");
-  //let li = document.createElement("li");
+  let li = document.createElement("li");
   //Jag trodde att create button är bättre än list? Vad tror ni?
-  let li = document.createElement("button");
+  //let li = document.createElement("button");
   ul.append(li);
   li.innerText = land.countryname;
   // print each land in the array of lands
