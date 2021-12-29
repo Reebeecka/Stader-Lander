46f810bf84b292c47407734af230226a75d6a870
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2021-12-29 22:38:00 +0100

    Added NEWS TO cities via API, just extra for fun

--- modules/lander.mjs
+++ modules/lander.mjs
@@ -60,7 +60,9 @@ export function switchColors(landName){
 
   for(let i = 0; i < liElements.length; i ++){
     liElements.item(i).style.color = "white";
-    landName.style.color="red";
+    liElements.item(i).style.border="none";
+    landName.style.color="#617bc4";
+    landName.style.border="3px solid white"
   }
 }
 
@@ -69,6 +71,9 @@ async function printCountry(land){
 
 let wikiURL = "https://sv.wikipedia.org/w/rest.php/v1/search/page?q=" + land.countryname + "&limit=1";
 let s = await ReadWikiAPI(wikiURL);
+
+let newsURL = "https://newsapi.org/v2/everything?q=" + land.countryname + "&from=2021-11-29&sortBy=publishedAt&apiKey=ce2d33c9daf84acbae294d547091fba1";
+    let n = await ReadWikiAPI(newsURL);
 
 // //Creates H1 element that writes out Country name
 let contryH1 = document.createElement("h1");
@@ -85,7 +90,25 @@ let description = document.createElement("p");
 description.innerHTML = "Till vänster ser du några av " + land.countryname + "s städer, tryck på en av dessa för mer infomration!"
 let section = document.getElementById("section");
 section.innerHTML = "";
+
+let section2 = document.getElementById("clear");
+    section2.innerHTML="";
+
+    let title = document.createElement("h2");
+    title.innerHTML=n.articles[0].title;
+
+    let news = document.createElement("p");
+    news.innerHTML=n.articles[0].description;
+
+    let source = document.createElement("img");
+    source.src=n.articles[0].urlToImage;
+
+    let articleLink = document.createElement("a");
+    articleLink.innerHTML=n.articles[0].url;
+    articleLink.href=n.articles[0].url;
+
 section.append(contryH1, contryDescription, contryImg, description);
+section2.append(title, news, source, articleLink);
 }
 
 
